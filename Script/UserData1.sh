4d18a5f0652de815f7fc97a8228dc21c7e880143
root <root@ip-172-30-0-181.ec2.internal>
2019-02-01 00:00:56 +0000

    Subindo awscli

--- Script/UserData1.sh
+++ Script/UserData1.sh
@@ -8,6 +8,7 @@ apt-get update -y
 cd /usr/local
 git clone https://github.com/koga021/curso-aws.git
 cd curso-aws
+apt-get install awscli -y
 apt-get install nginx -y
 apt-get install python-pip -y
 
