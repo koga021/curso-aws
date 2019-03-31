b0aacbcb7619ab1c11b65f54c29c1be7a0b0ef34
douglas.ribas <douglas.ribas@mac221024.local>
2019-03-31 16:37:50 -0300

    Iniciando desenvolvimento de BE

---
+++ Script/StartBE.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+LoadBalancerFe=""
+LoadBalancerBe=""
+QueueAddress=""
+
+
+sudo su -
+apt-get update -y
+cd /usr/local
+git clone https://github.com/koga021/curso-aws.git
+cd curso-aws
+##apt-get install awscli -y
+#apt-get install nginx -y
+apt-get install python-pip -y
+
+pip install tornado
+pip install requests
+pip install jinja2
+pip install tornado
+
+#python Python/CreateIndexFe.py
+python Python/StartBE.py
+#python /usr/local/curso-aws/Python/CreateIndexFe.py
